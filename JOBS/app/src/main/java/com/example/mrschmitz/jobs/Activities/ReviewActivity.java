e5b39e69fe123e11e47668b49b2bd1cfec3dde3c
Noah Hessel <nwhessel@gmail.com>
2017-10-19 12:45:54 -0500

    Made review screen look better

--- JOBS/app/src/main/java/com/example/mrschmitz/jobs/Activities/ReviewActivity.java
+++ JOBS/app/src/main/java/com/example/mrschmitz/jobs/Activities/ReviewActivity.java
@@ -1,15 +1,37 @@
 package com.example.mrschmitz.jobs.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.mrschmitz.jobs.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import agency.tango.android.avatarview.views.AvatarView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    @BindView(R.id.avatar)
+    AvatarView avatarView;
+
+    @BindView(R.id.username)
+    TextView usernameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+        ButterKnife.bind(this);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        GlideApp.with(this)
+                .load(user.getPhotoUrl())
+                .fitCenter()
+                .into(avatarView);
+
+        usernameTextView.setText(user.getDisplayName());
     }
 }
