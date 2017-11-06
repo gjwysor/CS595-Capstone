3fc20d60bc8f3b2c7d7ddc20f6d583daba6ceba0
VictorYang <vhyang@uwm.edu>
2017-11-06 16:26:11 -0600

    Latest UI
    All the UI has been updated to have the bottom navigation bar so we are able to go navigate through all of the activities.
    
    I have updated all of the information in the activites that I was presented with.

--- JOBS/app/src/main/java/com/example/mrschmitz/jobs/Activities/JobListActivity.java
+++ JOBS/app/src/main/java/com/example/mrschmitz/jobs/Activities/JobListActivity.java
@@ -1,22 +1,67 @@
 package com.example.mrschmitz.jobs.Activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import com.example.mrschmitz.jobs.Activities.Utilities.BottomNavigationViewHelper;
 import com.example.mrschmitz.jobs.R;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.ArrayList;
+
 
 public class JobListActivity extends AppCompatActivity {
+    private static final String TAG = "JobListActivity";
+    private static final int ACTIVITY_NUM = 1;
 
+    private Context mContext = JobListActivity.this;
+
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_list);
+        Log.d(TAG, "onCreate: started.");
+
+        listView = findViewById(R.id.list_view);
+        ArrayList<String> theList = new ArrayList<>();
+        theList.add("Clean my gutters");
+        theList.add("Rotate my tires!");
+        theList.add("PLZ help me move!");
+        theList.add("Volunteer for cleaning the rivers");
+        theList.add("Cut my grass.");
+        int counter = 0;
+        while(counter < theList.size()){
+            theList.add(theList.get(counter));
+        }
+        ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, theList);
+        listView.setAdapter(listAdapter);
+        setupBottomNavigationView();
     }
 
     public void backgoogle(View view){
         Intent intent = new Intent(JobListActivity.this, MapActivity.class);
         startActivity(intent);
+    }
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
     }
 }
--- JOBS/app/src/main/java/com/example/mrschmitz/jobs/Activities/PostJobActivity.java
+++ JOBS/app/src/main/java/com/example/mrschmitz/jobs/Activities/PostJobActivity.java
@@ -1,18 +1,23 @@
 package com.example.mrschmitz.jobs.Activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.mrschmitz.jobs.Activities.Utilities.BottomNavigationViewHelper;
 import com.example.mrschmitz.jobs.R;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
-
-import static com.example.mrschmitz.jobs.R.layout.activity_post_job;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class PostJobActivity extends AppCompatActivity {
+
     public static class jobOb{
         private String jobTitle;
         private String jobSkills;
@@ -43,29 +48,46 @@ public class PostJobActivity extends AppCompatActivity {
         }
     }
 
+
+    private static final String TAG = "PostJobActivity";
+    private static final int ACTIVITY_NUM = 2;
+
+    private Context mContext = PostJobActivity.this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity_post_job);
+        setContentView(R.layout.activity_post_job);
+
+        Log.d(TAG, "onCreate: starting.");
+
+        setupBottomNavigationView();
     }
-
-
 
     public void backToProfile(View view){
-        startActivity(new Intent(PostJobActivity.this, ProfileActivity.class));
+        Intent intent = new Intent(PostJobActivity.this, ProfileActivity.class);
+        startActivity(intent);
     }
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+    public void postJob(View view){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
 
-    public void postJob(View view){		
-        // Write a message to the database		
-        FirebaseDatabase database = FirebaseDatabase.getInstance();		
-        DatabaseReference myRef = database.getReference();		
-		
-        EditText title = findViewById(R.id.editText3);		
-        EditText skills = findViewById(R.id.editText4);		
-        EditText desc = findViewById(R.id.editText5);		
-		
-        jobOb refJob = new jobOb(title.getText().toString(), skills.getText().toString(), desc.getText().toString());		
-		
-        myRef.child("Jobs").push().setValue(refJob);		
-      }
+        EditText title = findViewById(R.id.editText3);
+        EditText skills = findViewById(R.id.editText4);
+        EditText desc = findViewById(R.id.editText5);
+
+        jobOb refJob = new jobOb(title.getText().toString(), skills.getText().toString(), desc.getText().toString());
+
+        myRef.child("Jobs").push().setValue(refJob);
+    }
 }
